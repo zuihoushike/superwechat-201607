a4ca7234113946a7e7821d8fc86a251c87a49073
kiril <m15229705442@163.com>
2016-11-09 03:27:49 +0800

    新的朋友界面，设置已同意显示

--- ChatDemoUI3.0/src/cn/ypxy/superwechat/adapter/NewFriendsMsgAdapter.java
+++ ChatDemoUI3.0/src/cn/ypxy/superwechat/adapter/NewFriendsMsgAdapter.java
@@ -164,20 +164,24 @@ public class NewFriendsMsgAdapter extends ArrayAdapter<InviteMessage> {
 					}
 				});
 			} else if (msg.getStatus() == InviteMesageStatus.AGREED) {
+				holder.status.setVisibility(View.VISIBLE);
 				holder.status.setText(str5);
 				holder.status.setBackgroundDrawable(null);
 				holder.status.setEnabled(false);
 			} else if(msg.getStatus() == InviteMesageStatus.REFUSED){
+				holder.status.setVisibility(View.VISIBLE);
 				holder.status.setText(str6);
 				holder.status.setBackgroundDrawable(null);
 				holder.status.setEnabled(false);
 			} else if(msg.getStatus() == InviteMesageStatus.GROUPINVITATION_ACCEPTED){
 			    String str = msg.getGroupInviter() + str9 + msg.getGroupName();
+				holder.status.setVisibility(View.VISIBLE);
                 holder.status.setText(str);
                 holder.status.setBackgroundDrawable(null);
                 holder.status.setEnabled(false);
             } else if(msg.getStatus() == InviteMesageStatus.GROUPINVITATION_DECLINED){
                 String str = msg.getGroupInviter() + str10 + msg.getGroupName();
+				holder.status.setVisibility(View.VISIBLE);
                 holder.status.setText(str);
                 holder.status.setBackgroundDrawable(null);
                 holder.status.setEnabled(false);
