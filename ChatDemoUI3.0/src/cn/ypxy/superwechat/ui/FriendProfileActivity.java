afc51faffe4cd7b1522a82bb8cd35daba9446153
kiril <m15229705442@163.com>
2016-11-09 22:10:30 +0800

    添加保护,避免只有环信好友关系的用户进入详情界面出现空指针问题

--- ChatDemoUI3.0/src/cn/ypxy/superwechat/ui/FriendProfileActivity.java
+++ ChatDemoUI3.0/src/cn/ypxy/superwechat/ui/FriendProfileActivity.java
@@ -49,6 +49,7 @@ public class FriendProfileActivity extends BaseActivity {
         user = (User) getIntent().getSerializableExtra(I.User.USER_NAME);
         if (user==null){
             MFGT.finish(this);
+            return;
         }
         initView();
     }
