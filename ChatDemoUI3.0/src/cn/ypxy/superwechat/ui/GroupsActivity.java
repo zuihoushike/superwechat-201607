76f685dfdaf5c33455ddc600c664fec351d66e74
kiril <m15229705442@163.com>
2016-11-13 21:26:45 +0800

    同意新建群组相关页面的风格

--- ChatDemoUI3.0/src/cn/ypxy/superwechat/ui/GroupsActivity.java
+++ ChatDemoUI3.0/src/cn/ypxy/superwechat/ui/GroupsActivity.java
@@ -13,9 +13,11 @@
  */
 package cn.ypxy.superwechat.ui;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.support.v4.app.NotificationCompat;
 import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v4.widget.SwipeRefreshLayout.OnRefreshListener;
 import android.view.MotionEvent;
@@ -25,12 +27,16 @@ import android.view.WindowManager;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemClickListener;
+import android.widget.ImageView;
 import android.widget.ListView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.hyphenate.chat.EMClient;
 import com.hyphenate.chat.EMGroup;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
 import cn.ypxy.superchat.R;
 import cn.ypxy.superwechat.Constant;
 
@@ -41,29 +47,102 @@ import java.util.List;
 
 public class GroupsActivity extends BaseActivity {
 	public static final String TAG = "GroupsActivity";
-	private ListView groupListView;
+	@BindView(R.id.img_back)
+	ImageView mImgBack;
+	@BindView(R.id.txt_title)
+	TextView mTxtTitle;
+	@BindView(R.id.list)
+	ListView mList;
+	@BindView(R.id.swipe_layout)
+	SwipeRefreshLayout mSwipeLayout;
 	protected List<EMGroup> grouplist;
 	private GroupAdapter groupAdapter;
 	private InputMethodManager inputMethodManager;
 	public static GroupsActivity instance;
 	private View progressBar;
-	private SwipeRefreshLayout swipeRefreshLayout;
 	
-	
-	Handler handler = new Handler(){
-	    public void handleMessage(android.os.Message msg) {
-	        swipeRefreshLayout.setRefreshing(false);
-	        switch (msg.what) {
-            case 0:
-                refresh();
-                break;
-            case 1:
-                Toast.makeText(GroupsActivity.this, R.string.Failed_to_get_group_chat_information, Toast.LENGTH_LONG).show();
-                break;
+	Handler handler = new Handler() {
+		public void handleMessage(NotificationCompat.MessagingStyle.Message msg) {
+			mSwipeLayout.setRefreshing(false);
+			switch (msg.what) {
+				case 0:
+					refresh();
+					break;
+				case 1:
+					Toast.makeText(GroupsActivity.this, R.string.Failed_to_get_group_chat_information, Toast.LENGTH_LONG).show();
+					break;
+				default:
+					break;
+			}
+		}
+	};
+			
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.em_fragment_groups);
+		ButterKnife.bind(this);
+		instance = this;
+		inputMethodManager = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+		grouplist = EMClient.getInstance().groupManager().getAllGroups();
+		initView();
+		setListener();
+	}
 
-            default:
-                break;
-            }
+	private void initView() {
+	}
+
+	private void setListener() {
+		//pull down to refresh
+		mSwipeLayout.setOnRefreshListener(new OnRefreshListener() {
+			
+			@Override
+			public void onRefresh() {
+				new Thread() {
+					@Override
+					public void run() {
+						try {
+							EMClient.getInstance().groupManager().getJoinedGroupsFromServer();
+							handler.sendEmptyMessage(0);
+						} catch (HyphenateException e) {
+							e.printStackTrace();
+							handler.sendEmptyMessage(1);
+						}
+					}
+				}.start();
+			}
+		});
+		mList.setOnItemClickListener(new OnItemClickListener() {
+			
+			@Override
+			public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+				if (position == 1) {
+					// create a new group
+					MFGT.gotoCreateNewGroup(GroupsActivity.this);
+				} else if (position == 2) {
+					// join a public group
+					MFGT.gotoPublicGroup(GroupsActivity.this);
+				} else {
+					// enter group chat
+					Intent intent = new Intent(GroupsActivity.this, ChatActivity.class);
+					// it is group chat
+					intent.putExtra("chatType", Constant.CHATTYPE_GROUP);
+					intent.putExtra("userId", groupAdapter.getItem(position - 3).getGroupId());
+					startActivityForResult(intent, 0);
+				}
+			}
+			
+		});
+		mList.setOnTouchListener(new OnTouchListener() {
+			@Override
+			public boolean onTouch(View v, MotionEvent event) {
+				if (getWindow().getAttributes().softInputMode != WindowManager.LayoutParams.SOFT_INPUT_STATE_HIDDEN) {
+					if (getCurrentFocus() != null)
+						inputMethodManager.hideSoftInputFromWindow(getCurrentFocus().getWindowToken(), 
+								InputMethodManager.HIDE_NOT_ALWAYS);
+				}
+				return false;
+			}
 	    }
 	};
 
